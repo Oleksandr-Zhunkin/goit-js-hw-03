c8f42d9369245d6a880bdcd58f982d64f8ba232c
Oleksandr_Zhynkin <godinsighted@gmail.com>
2024-04-24 23:26:09 +0300

    Fix task 2

--- js/task-2.js
+++ js/task-2.js
@@ -1,11 +1,19 @@
 function makeArray(firstArray = [], secondArray = [], maxLength = 15) {
-  if (
-    !Array.isArray(firstArray) ||
-    !Array.isArray(firstArray) ||
-    typeof maxLength !== 'number'
-  )
-    return 'First argument is not array or second argument is not array or last argument is not number!';
-
+  //
+  // Start validation of input data
+  if (!Array.isArray(firstArray)) {
+    return 'Error.First argument is not array!';
+  } else if (!Array.isArray(secondArray)) {
+    return 'Error.Second argument is not array!';
+  } else if (typeof maxLength !== 'number') {
+    return 'Error.Last argument is not number!';
+  } else if (maxLength < 0) {
+    return 'Error.The last argument is negative number!';
+  } else if (maxLength % 1 !== 0) {
+    return 'Error.The last argument is not an integer!';
+  }
+  // End validation of input data
+  //
   const mainArray = firstArray.concat(secondArray);
 
   if (mainArray.length > maxLength) {
@@ -21,4 +29,4 @@ console.log(makeArray(['Mango'], ['Ajax', 'Chelsea', 'Poly', 'Houston'], 3)); //
 console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 2)); // ["Earth", "Jupiter"]
 console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
 console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus', 'Venus'], 0)); // []
-console.log(makeArray(23, 35, 34, 67, 8, true));
+console.log(makeArray([23], [67], -1.1));
